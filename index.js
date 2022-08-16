1b6b8664546cd2550af8942b3e00b13be6c14876
Allan <allan.kimurgor@student.moringaschool.com>
2022-08-16 19:57:07 +0300

    Phase-1 Control flow lab

--- index.js
+++ index.js
@@ -1,11 +1,27 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(ride){
+ if (ride <= 400) {
+  return "This one is on me!";
+} 
+  else if (ride > 2500) {
+    return "No can do.";
+  }
+  else if(ride > 2000) {
+    return "I will gladly take your thirty bucks."
 }
-
-function ternaryCheckCity(){
-  // Write your code here!
 }
-
-function switchOnCharmFromTip(){
-  // Write your code here!
+function ternaryCheckCity(city) {
+const findCity = city === "NYC" ? "Ok, sounds good." : "No go."
+ return findCity
+}
+function switchOnCharmFromTip(output){
+  switch(output) {
+    case (output) = "generous" :
+      return "Thank you so much.";
+    break;
+    case (output) = "not as generous" :
+      return "Thank you.";
+    break;
+    default:
+      return "Bye.";
+  }
 }
